903442866adc84815497f45e03498dc53ecd0b56
gurudasjadhav451 <51242339+gurudasjadhav451@users.noreply.github.com>
2021-08-18 11:25:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-//This is test 
+//This is test action
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
